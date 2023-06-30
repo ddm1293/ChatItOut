ebb483423319a9189550a2298b605daa7a7bed2f
mk3790 <clara_park_ubc@thecdm.ca>
2023-06-30 16:27:34 -0700

    completed the stage transfer function

--- frontend/src/components/Chatbot.js
+++ frontend/src/components/Chatbot.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useRef, useCallback} from 'react';
 import send from "../assets/icon_send.png";
 import axios from 'axios';
 import stagearrow from "../assets/icon_stagearrow.png";
+import stagecomplete from "../assets/icon_stagecomplete.png";
 
 const serverURL = "http://localhost:5000";
 
@@ -24,17 +25,17 @@ export default function Chatbot() {
     // const [messages, setMessages] = useState([]);
     const [messages, setMessages] = useState({invitation: [], connection: [], exchange: [], agreement: [], reflection: []});
     const [stage, transitionStage] = useState(Stage.Invitation);
-    const [invStage, setInvStage] = useState("notStarted", "inProgress", "completed");
-    const [conStage, setConStage] = useState("notStarted", "inProgress", "completed");
-    const [excStage, setExcStage] = useState("notStarted", "inProgress", "completed");
-    const [agrStage, setAgrStage] = useState("notStarted", "inProgress", "completed");
-    const [refStage, setRefStage] = useState("notStarted", "inProgress", "completed");
+    const [invStage, setInvStage] = useState("inProgress");
+    const [conStage, setConStage] = useState("notStarted");
+    const [excStage, setExcStage] = useState("notStarted");
+    const [agrStage, setAgrStage] = useState("notStarted");
+    const [refStage, setRefStage] = useState("notStarted");
 
     const isInitialMount = useRef(true);
 
     const generateResponse = () => {
-        let responses = ["Hello, how are you?", "That is a bad idea.", "You are very intelligent!", "I'm sorry to hear that.", "Do you feel like a human being today?"];
-        let i = Math.floor((Math.random() * 5));
+        let responses = ["Hello, how are you?", "That is a bad idea.", "You are very intelligent!"];
+        let i = Math.floor((Math.random() * 3));
         if (i === 2) {
             advanceStage();
         }
@@ -61,17 +62,17 @@ export default function Chatbot() {
         const userInput = content.target.userInput.value;
         const chatbotMessage = generateResponse();
 
-        let stageMessages = messages[stage.name];
-        stageMessages.push({ type: 'user', message:userInput });
-        stageMessages.push({ type: 'chatbot', message:chatbotMessage });
+        if (stage.name !== "complete") {
+            let stageMessages = messages[stage.name];
+            stageMessages.push({ type: 'user', message:userInput });
+            stageMessages.push({ type: 'chatbot', message:chatbotMessage });
 
-        // just for testing
-        setInvStage("inProgress");
+            setMessages({
+                ...messages,
+                [stage.name]: stageMessages
+            });
+        }       
 
-        setMessages({
-            ...messages,
-            [stage.name]: stageMessages
-        });
         
         content.target.userInput.value="";
 
@@ -104,7 +105,7 @@ export default function Chatbot() {
                 nextStage = Stage.Complete;
                 setAgrStage("completed");
                 setRefStage("inProgress");
-                break;
+                break;  
             default:
                 nextStage = Stage.Complete;
         }
@@ -136,11 +137,27 @@ export default function Chatbot() {
          }
     }, [messages, saveToDisk]);
 
+    const getAllMessages = () => {
+        let arr = [];
+        let results = arr.concat(Object.values(messages));
+        return results.flat();
+    }
+
+    const wordColor = (stage) => {
+        if (stage === "notStarted") {
+            return 'text-white opacity-50 font-normal';
+        } else if (stage === "inProgress") {
+            return 'text-white font-medium';
+        } else {
+            return 'text-white font-normal';
+        }
+    }
+
     return (
         <>
         <div className="absolute top-28 w-2/3 right-24 vg-[#1e1e1e] rounded-lg">
             <div className="mb-4 max-h-[600px] overflow-y-auto">
-            {messages[stage.name].map((message, index) => ( // TODO: it currently erases previous stages msgs
+            { getAllMessages().map((message, index) =>  ( 
                 <div
                     key={index}
                     className={`mb-4 p-4 ${
@@ -171,63 +188,59 @@ export default function Chatbot() {
 
         {/* Left Status Bar */}
         <div className="absolute w-4/5 h-20 bg-[#242424] flex right-0 top-0">
-            <div className={`flex absolute top-8 left-0 w-64 h-12 border-b-4 ${invStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></div>
+            <span className={`flex absolute top-8 left-0 w-64 h-12 border-b-4 ${invStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></span>
             <div className="flex absolute top-8 left-20">
-                <span className="w-4 h-4 bg-white opacity-50 rounded-full"></span>
-                <span className="absolute left-1 -top-1 font-calibri font-normal text-14 leading-17 text-black">
-                    1
-                </span>
-                <span className="absolute left-8 -top-2 font-normal text-lg leading-22 text-white opacity-50 font-calibri">
+                <span className={`w-4 h-4 rounded-full ${invStage === "notStarted" ? "opacity-50 bg-white" : "opacity-100 bg-[#9adbff]"}`}> </span>
+                <span className={`absolute left-1 -top-1 font-calibri text-14 leading-17 ${invStage === "completed" ? "opacity-0" : "text-black"}`}> 1 </span> 
+                {invStage === "completed" ? <img src={stagecomplete} className="absolute left-0 top-0 rounded-full w-4 h-4" alt="Stage Complete" /> : null}
+                <span className={`absolute left-8 -top-2 text-lg leading-22 font-calibri ${wordColor(invStage)}`}>
                     Invitation
                 </span>
-                <img src={stagearrow} className="absolute left-40 top-1 rounded-full" />
+                <img src={stagearrow} className="absolute left-40 top-1 rounded-full" alt="Stage Arrow" />
             </div>
 
-            <div className={`flex absolute top-8 left-64 w-64 h-12 border-b-4 ${conStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></div>
-            <div className="absolute w-4 h-4 top-8 left-80 bg-white opacity-50 rounded-full"> 
-                <span className="absolute inset-x-0 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 text-black">
-                    2
-                </span>
-                <span className="absolute left-8 -top-2 font-normal text-lg leading-22 text-white font-calibri">
+            <span className={`flex absolute top-8 left-64 w-64 h-12 border-b-4 ${conStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></span>
+            <div className="flex absolute top-8 left-80"> 
+                <span className={`w-4 h-4 rounded-full ${conStage === "notStarted" ? "opacity-50 bg-white" : "opacity-100 bg-[#9adbff]"}`}> </span>
+                <span className={`absolute left-1 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 ${conStage === "completed" ? "opacity-0" : "text-black"}`}>2</span>
+                {conStage === "completed" ? <img src={stagecomplete} className="absolute left-0 top-0 rounded-full w-4 h-4" alt="Stage Complete" /> : null} 
+                <span className={`absolute left-8 -top-2 text-lg leading-22 font-calibri ${wordColor(conStage)}`}>
                     Connection
                 </span>
-                <img src={stagearrow} className="absolute left-44 top-1 rounded-full" />
+                <img src={stagearrow} className="absolute left-44 top-1 rounded-full" alt="Stage Arrow" />
             </div>
 
-            <div className={`flex absolute top-8 left-[500px] w-64 h-12 border-b-4 ${excStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></div>
-            <span style={{ left: '560px' }}
-                  className="absolute w-4 h-4 top-8 bg-white opacity-50 rounded-full">
-                  <span className="absolute inset-x-0 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 text-black">
-                    3
-                    </span>
-                    <span className="absolute left-8 -top-2 font-normal text-lg leading-22 text-white font-calibri">
-                        Exchange
-                    </span>
-                    <img src={stagearrow} className="absolute left-44 top-1 rounded-full" />
-            </span>
+            <span className={`flex absolute top-8 left-[500px] w-64 h-12 border-b-4 ${excStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></span>
+            <div className="flex absolute top-8" style={{ left: '560px' }}>
+                <span className={`w-4 h-4 rounded-full ${excStage === "notStarted" ? "opacity-50 bg-white" : "opacity-100 bg-[#9adbff]"}`}> </span>
+                <span className={`absolute left-1 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 ${excStage === "completed" ? "opacity-0" : "text-black"}`}>3</span>
+                {excStage === "completed" ? <img src={stagecomplete} className="absolute left-0 top-0 rounded-full w-4 h-4" alt="Stage Complete" /> : null}  
+                <span className={`absolute left-8 -top-2 text-lg leading-22 font-calibri ${wordColor(excStage)}`}>
+                    Exchange
+                </span>
+                <img src={stagearrow} className="absolute left-44 top-1 rounded-full" alt="Stage Arrow" />
+            </div>
 
             <div className={`flex absolute top-8 left-[750px] w-60 h-12 border-b-4 ${agrStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></div>
-            <span style={{ left: '800px' }}
-                  className="absolute w-4 h-4 top-8 bg-white opacity-50 rounded-full">
-                  <span className="absolute inset-x-0 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 text-black">
-                    4
-                    </span>
-                    <span className="absolute left-8 -top-2 font-normal text-lg leading-22 text-white font-calibri">
+            <div className="flex absolute top-8" style={{ left: '800px' }}>
+                    <span className={`w-4 h-4 rounded-full ${agrStage === "notStarted" ? "opacity-50 bg-white" : "opacity-100 bg-[#9adbff]"}`}> </span>
+                    <span className={`absolute left-1 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 ${agrStage === "completed" ? "opacity-0" : "text-black"}`}>4</span> 
+                    {agrStage === "completed" ? <img src={stagecomplete} className="absolute left-0 top-0 rounded-full w-4 h-4" alt="Stage Complete" /> : null} 
+                    <span className={`absolute left-8 -top-2 text-lg leading-22 font-calibri ${wordColor(agrStage)}`}>
                         Agreement
                     </span>
-                    <img src={stagearrow} className="absolute left-44 top-1 rounded-full" />
-            </span>
+                    <img className="absolute left-44 top-1 rounded-full" src={stagearrow} alt="Stage Arrow" />
+            </div>
 
             <div className={`flex absolute top-8 left-[990px] w-64 h-12 border-b-4 ${refStage === "inProgress" ? "border-[#9adbff]" : "border-none"}`}></div>
-            <span style={{ left: '1040px' }}
-                  className="absolute w-4 h-4 top-8 bg-white opacity-50 rounded-full">
-                  <span className="absolute inset-x-0 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 text-black">
-                    5
-                    </span>
-                    <span className="absolute left-8 -top-2 font-normal text-lg leading-22 text-white font-calibri">
+            <div className="flex absolute top-8" style={{ left: '1040px' }}>
+                    <span className={`w-4 h-4 rounded-full ${refStage === "notStarted" ? "opacity-50 bg-white" : "opacity-100 bg-[#9adbff]"}`}> </span>
+                    <span className={`absolute left-1 flex items-center justify-center -top-1 font-calibri font-normal text-14 leading-17 ${refStage === "completed" ? "opacity-0" : "text-black"}`}>5</span>
+                    {refStage === "completed" ? <img src={stagecomplete} className="absolute left-0 top-0 rounded-full w-4 h-4" alt="Stage Complete" /> : null}  
+                    <span className= {`absolute left-8 -top-2 text-lg leading-22 font-calibri ${wordColor(refStage)}`}>
                         Reflection
                     </span>
-            </span>
+            </div>
             </div>
         </>
     )
