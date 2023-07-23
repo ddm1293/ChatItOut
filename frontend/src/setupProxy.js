5918a7420a59eec6dfba37f067ba57291f24e0d2
korams <korams@students.cs.ubc.ca>
2023-07-22 17:26:29 -0700

    update serverURL

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api/**',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://chatitout-chatbot.onrender.com',
       onProxyRes: function (proxyRes, req, res) {
         proxyRes.headers['Access-Control-Allow-Origin'] = 'https://chatitout.onrender.com';
       },
