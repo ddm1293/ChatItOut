bd6c539a0a575f862884529a8bd87bf9fb7e8727
korams <korams@students.cs.ubc.ca>
2023-07-31 23:32:33 -0700

    add comments to server.py

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,17 @@
-from flask import Flask, make_response
+from flask import Flask
 from flask import request
 from chatbot.chatbot_tokens import *
 
 app = Flask(__name__)
 
+# Passes user input to chatbot and passes response to frontend
 @app.post('/home/chat')
 def talk_to_ai():
     data = request.json
     ai_resp = generate_response(data)
     return ai_resp
 
+# Add headers that prevent requests being blocked
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', 'https://chatitout.onrender.com')
