d07eb9df9de5e037c7aaea3b6325960be60e6747
dingmandeng <makemake1293@gmail.com>
2024-03-05 16:49:07 -0800

    add redux; write basic store and slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+import { store } from './slices/store.js'
+import { Provider } from 'react-redux';
 
 // Turn off development tools in production mode
 if (process.env.NODE_ENV === 'production') {
@@ -12,7 +14,9 @@ if (process.env.NODE_ENV === 'production') {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
